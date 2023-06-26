c2649dcefad8edcb3dccf7617b48de4670ad6cdb
Jess Mann <me@jessmann.io>
2023-06-25 21:19:12 -0500

    Make calendar heatmap work

--- src/forecasting/chart.ts
+++ src/forecasting/chart.ts
@@ -1,12 +1,28 @@
-import { format } from "date-fns";
+import { eachDayOfInterval, endOfDay, format, toDate } from "date-fns";
 import { percentiles } from "../common/math";
 import { runMonteCarlo } from "./montecarlo";
 import { readIntoForecastingData } from "./reader";
 
-type ShortDate = string;
+type ShortDate = string; // yyyy-mm-dd
+
+/**
+ * @internal
+ */
+export function toCalendarData(
+  from: number,
+  to: number,
+  value: number,
+): { date: string; value: number }[] {
+  const start = endOfDay(from);
+  const end = endOfDay(to);
+  return eachDayOfInterval({ start, end }).map((d) => ({
+    date: format(d, "yyyy-MM-dd"),
+    value,
+  }));
+}
 
 export async function showAsCalendar(filepath: string): Promise<{
-  calendarData: { date: number; value?: number }[];
+  calendarData: { date: string; value?: number }[];
   remainingStories: number;
   p50: ShortDate;
   p85: ShortDate;
@@ -16,20 +32,22 @@ export async function showAsCalendar(filepath: string): Promise<{
 
   const results = runMonteCarlo(10000, remaining, throughput).sort();
   const {
+    [20]: p20,
     [50]: p50,
     [85]: p85,
     [95]: p95,
     [100]: p100,
-  } = percentiles(results, 50, 85, 95, 100);
+  } = percentiles(results, 20, 50, 85, 95, 100);
+  const _calendarData = [
+    ...toCalendarData(Date.now(), p20, 10),
+    ...toCalendarData(p20, p50, 35),
+    ...toCalendarData(p50, p85, 60),
+    ...toCalendarData(p85, p95, 90),
+    ...toCalendarData(p95, p100, 96),
+  ];
 
   return {
-    calendarData: [
-      { date: new Date().getTime() },
-      { date: p50, value: 50 },
-      { date: p85, value: 85 },
-      { date: p95, value: 95 },
-      { date: p100, value: 100 },
-    ],
+    calendarData: _calendarData,
     remainingStories: remaining,
     p50: format(p50, "MM/dd/yy"),
     p85: format(p85, "MM/dd/yy"),
