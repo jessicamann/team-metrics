41b4ffe3594a7bd80296866530d26ec824e31a29
Jess Mann <me@jessmann.io>
2023-07-03 08:57:53 -0500

    Remove unused function

--- src/throughput/index.ts
+++ src/throughput/index.ts
@@ -1,14 +1,5 @@
-import { getById } from "@app/common/repository";
 import { showAsLineChart } from "./chart";
 import { byWeek } from "./durations";
 import { intoThroughput } from "./reader";
-import { ThroughputData } from "./type";
 
-async function toWeeklyThroughput(id: string): Promise<ThroughputData[]> {
-  const data = intoThroughput(getById(id));
-  return data
-    .toThroughput(byWeek)
-    .sort((a, b) => a.periodEnd.getTime() - b.periodEnd.getTime());
-}
-
-export { byWeek, intoThroughput, showAsLineChart, toWeeklyThroughput };
+export { byWeek, intoThroughput, showAsLineChart };
