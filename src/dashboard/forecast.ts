e9df8a8afc27772d19924f6497415c2a27171fe1
Jess Mann <me@jessmann.io>
2023-07-03 12:25:46 -0500

    Refactor dashboard feature forecast for readability

--- src/dashboard/forecast.ts
+++ src/dashboard/forecast.ts
@@ -2,7 +2,7 @@ import { percentiles } from "@app/common/math";
 import { InputData } from "@app/common/repository";
 import { runMonteCarlo } from "@app/forecasting";
 import { format, isAfter, subMonths } from "date-fns";
-import { compact, countBy, every, groupBy, map, some } from "lodash";
+import { chain, compact, countBy, every, groupBy, map, some } from "lodash";
 import { byWeek, intoThroughput } from "@app/throughput";
 
 type Summary = {
@@ -54,6 +54,31 @@ function forecast(remaining: number, throughput: number[]) {
   return percentiles(simulations, 50, 85, 95);
 }
 
+function toFeatureSummary(options: Options, throughput: number[]) {
+  return function (value: Forecastable[], featureName: string) {
+    if (options.onlyRecent && isNotRecent(value)) {
+      return;
+    }
+
+    const completed = countBy(value, "completed")["true"];
+    const incomplete = countBy(value, "completed")["false"];
+    const forecastConfidence = forecast(incomplete, throughput);
+
+    return {
+      name: featureName,
+      progress: {
+        completed: completed,
+        total: value.length,
+      },
+      forecast: {
+        p50: format(forecastConfidence[50], "MM/dd/yy"),
+        p85: format(forecastConfidence[85], "MM/dd/yy"),
+        p95: format(forecastConfidence[95], "MM/dd/yy"),
+      },
+    };
+  };
+}
+
 export function forecastSummary(
   data: InputData[],
   options: Options,
@@ -61,30 +86,10 @@ export function forecastSummary(
   const throughput = intoThroughput(data)
     .count(byWeek)
     .map((d) => d.total);
-  const forecastableByFeature = groupBy(fromInput(data), "feature");
 
-  return compact(
-    map(forecastableByFeature, (value, key) => {
-      if (options.onlyRecent && isNotRecent(value)) {
-        return;
-      }
-
-      const completed = countBy(value, "completed")["true"];
-      const incomplete = countBy(value, "completed")["false"];
-      const forecastConfidence = forecast(incomplete, throughput);
-
-      return {
-        name: key,
-        progress: {
-          completed: completed,
-          total: value.length,
-        },
-        forecast: {
-          p50: format(forecastConfidence[50], "MM/dd/yy"),
-          p85: format(forecastConfidence[85], "MM/dd/yy"),
-          p95: format(forecastConfidence[95], "MM/dd/yy"),
-        },
-      };
-    }),
-  );
+  return chain(fromInput(data))
+    .groupBy("feature")
+    .map(toFeatureSummary(options, throughput))
+    .compact()
+    .value();
 }
