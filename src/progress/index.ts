f6dfdc431006cdde421fefda342bbf9e1d93d686
Jess Mann <me@jessmann.io>
2023-07-03 11:01:14 -0500

    Remove unused files

--- src/progress/index.ts
+++ src/progress/index.ts
@@ -1,6 +1,3 @@
-import { showAsDonutChartsByFeature } from "./chart";
-
-export { showAsDonutChartsByFeature };
 export { countByStatus, groupByFeature } from "./calculate";
 export { StoryData, Options } from "./type";
 export { intoProgressData } from "./reader";
