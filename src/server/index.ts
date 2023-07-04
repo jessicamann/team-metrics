735d9effaa81297914782329844d878096200266
Jess Mann <me@jessmann.io>
2023-07-04 15:54:54 -0500

    Specify build and deploy in config file
    Co-authored-by: Rafael Ferreira <rafa@rafasf.com>

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,19 +4,9 @@ import fastifyView from "@fastify/view";
 import fastifyAutoload from "@fastify/autoload";
 import fastifyMultipart from "@fastify/multipart";
 
-const loggerconfig = () => ({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      translateTime: "HH:MM:ss Z",
-      ignore: "pid,hostname",
-    },
-  },
-});
-
 export function buildServer({ logger = true } = {}) {
   const fastify = Fastify({
-    logger: logger ? loggerconfig() : false,
+    logger: logger,
   });
 
   fastify.register(fastifyView, {
