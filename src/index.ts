a0b8995e30a19865532ad9e5db8eeabcfcfc6e8d
Jess Mann <me@jessmann.io>
2023-07-04 14:52:01 -0500

    Deploy this code!
    Co-authored-by: Rafael Ferreira <rafa@rafasf.com>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { buildServer } from "./server";
 
+const port = parseInt(process.env.PORT || "3000");
+
 (async () => {
   const server = buildServer();
   try {
-    await server.listen({ port: 3000 });
+    await server.listen({ port: port });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
