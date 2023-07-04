2de02181dd1830d2c40c453b4cb1893e5c16b4af
Jess Mann <me@jessmann.io>
2023-07-04 15:26:10 -0500

    Listen on the host that Railway wants

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const port = parseInt(process.env.PORT || "3000");
 (async () => {
   const server = buildServer();
   try {
-    await server.listen({ port: port });
+    await server.listen({ host: "0.0.0.0", port: port });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
