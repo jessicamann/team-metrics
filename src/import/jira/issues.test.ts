05344af1694eeda4d9ec8f3c07a54218d6713d81
Rafael Ferreira <rafa@rafasf.com>
2023-07-16 21:10:05 -0500

    Use Testcontainers for stubbing dependencies
    First usecase is stubbing Jira with mountebank.
    
    type: test
    area: infra

--- src/import/jira/issues.test.ts
+++ src/import/jira/issues.test.ts
@@ -1,10 +1,30 @@
 import { Version3Client } from "jira.js";
 import { allIssues } from "./issues";
+import { GenericContainer, StartedTestContainer } from "testcontainers";
+import path from "path";
+
+const SECONDS = 1000;
 
 describe("Jira Issue Import", () => {
-  it.skip("pending stub setup - fetches all issues for the query provided", async () => {
+  let container: StartedTestContainer;
+
+  beforeAll(async () => {
+    container = await new GenericContainer("bbyars/mountebank:2.8.2")
+      .withExposedPorts(4545)
+      .withCopyFilesToContainer(
+        ["issue-search.ejs", "one-issue.ejs"].map(toFileInFixtureDir),
+      )
+      .withCommand(["--configfile", "/stubs/issue-search.ejs"])
+      .start();
+  }, 70 * SECONDS);
+
+  afterAll(async () => {
+    await container.stop();
+  });
+
+  it("fetches all issues for the query provided", async () => {
     const client = new Version3Client({
-      host: "http://localhost:4545",
+      host: `http://localhost:${container.getMappedPort(4545)}`,
       authentication: {
         basic: {
           email: "grogu@example.com",
@@ -25,3 +45,10 @@ describe("Jira Issue Import", () => {
     expect(firstIssue).toHaveProperty("changelog");
   });
 });
+
+function toFileInFixtureDir(filename: string) {
+  return {
+    source: path.resolve(__dirname, "../../../fixtures/stub", filename),
+    target: `/stubs/${filename}`,
+  };
+}
